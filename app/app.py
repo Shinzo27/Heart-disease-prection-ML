ec20289b7ba12b7e5edf731b1505740b5801fc2c
Shinzo27 <20bmiit031@gmail.com>
2024-12-01 19:27:00 +0530

    api

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
+
+model = pickle.load(open('../model.pkl', 'rb'))
 
 @app.route('/')
 def index():
@@ -8,8 +12,14 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input = request.form.get('age')
-    return jsonify(input)
+    json_ = request.get_json()
+    print(json_)
+    query = pd.DataFrame([json_], columns=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal'])
+    prediction = model.predict(query)
+    print(prediction)
+    prediction_value = float(prediction[0])
+    print(prediction_value)
+    return jsonify({ 'prediction': prediction_value })
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
