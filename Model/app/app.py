364547b9deeed2111924f087a21e38b5cf7a9437
Shinzo27 <20bmiit031@gmail.com>
2024-12-02 11:06:56 +0530

    Model

--- Model/app/app.py
+++ Model/app/app.py
@@ -15,11 +15,11 @@ def predict():
     json_ = request.get_json()
     print(json_)
     query = pd.DataFrame([json_], columns=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal'])
-    prediction = model.predict(query)
+    prediction = model.predict_proba(query)
     print(prediction)
-    prediction_value = float(prediction[0])
-    print(prediction_value)
-    return jsonify({ 'prediction': prediction_value })
+    percentage = (prediction[0][1])*100 
+    print(percentage)
+    return jsonify({ 'prediction': percentage })
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
