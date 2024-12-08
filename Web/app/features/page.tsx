49b103aa7032f24d0b94e5cd4f2781f37e0cb609
Shinzo27 <20bmiit031@gmail.com>
2024-12-08 14:21:50 +0530

    Ai

--- Web/app/features/page.tsx
+++ Web/app/features/page.tsx
@@ -1,11 +1,11 @@
 "use client"
-
 import model from "@/lib/gemini";
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 const page = () => {
     const [response, setResponse] = useState("");
+    const [analysis, setAnalysis] = useState("");
 
     useEffect(()=>{
         async function generate(){
@@ -24,15 +24,59 @@ const page = () => {
             "ca": 0,
             "thal": 1
           }
-    
-          const prompt = `These are my health data: ${JSON.stringify(data)}. Based on this data, I am getting a risk of heart disease 83.78%. please provide me danger signs and recommendations to reduce my risk of heart disease.`;
+          const prompt = `You are an API for a health application. Always respond in JSON format with the following structure:
+              {
+                "status": "success",
+                "data": {
+                  "dietRecommendation": ["string"],
+                  "analysis": {
+                    "problemAreas": ["string"],
+                    "suggestions": ["string"]
+                  }
+                },
+                "error": "null or string"
+              }
+
+              Input data: {heartRate: {
+                age: ${data.age}\n
+                sex: ${data.sex}\n
+                cp: ${data.cp}\n
+                trestbps: ${data.trestbps}\n
+                chol: ${data.chol}\n
+                fbs: ${data.fbs}\n
+                restecg: ${data.restecg}\n
+                thalach: ${data.thalach}\n
+                exang: ${data.exang}\n
+                oldpeak: ${data.oldpeak}\n
+                slope: ${data.slope}\n
+                ca: ${data.ca}\n  
+              }}
+
+              Provide a response in this format.`
           const result = await model.generateContent([prompt]); 
           setResponse(result.response.text());
+          console.log(result.response.text());
+          setAnalysis(result.response.text());
         }
         generate();
       }, []);
+      
     return (
+      <>
         <ReactMarkdown>{response}</ReactMarkdown>
+        <div>
+          {
+            response.includes("data") &&
+            <div>
+              <h2>Analysis</h2>
+              <div>
+                  <ReactMarkdown>{analysis}</ReactMarkdown>
+              </div>
+            </div>
+          }
+        </div>
+      </>
+
     );
 }
 
