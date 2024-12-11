4f04ee0d31afccbc0d25e7f6476f04b87de2f680
Shinzo27 <20bmiit031@gmail.com>
2024-12-11 12:32:52 +0530

    Auth

--- Web/app/details/[result]/page.tsx
+++ Web/app/details/[result]/page.tsx
@@ -1,17 +1,15 @@
 import DetailsComponent from "@/components/DetailsComponent";
-import ResultComponent from "@/components/ResultComponent";
 import { getHealthMetrics, getPrediction } from "@/lib/db";
 import MarkdownIt from "markdown-it";
 
 export default async function Page({ params }: { params: Promise<{ result: number }> }) {
-    const getData = await getHealthMetrics(2)
-
+    const getData = await getHealthMetrics(3)
     const predict = await getPrediction(getData);
     const md = new MarkdownIt();
     const parse = md.parse(predict, {})
     const result = JSON.parse(parse[0].content)
-
+    console.log(getData);
     return (
-      <DetailsComponent result={result} />
+      <DetailsComponent result={result} assessmentResult={getData} />
     )
 }
--- Web/lib/auth.ts
+++ Web/lib/auth.ts
@@ -1,66 +1,67 @@
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { prisma } from '@/lib/prisma';
-// import bcrypt from 'bcryptjs';
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from '@/lib/prisma';
+import bcrypt from 'bcryptjs';
 
-// export const NEXT_AUTH = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         username: { label: "Username", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials: any) {
-//         if (!credentials?.email || !credentials?.password) {
-//           return null;
-//         }
-//         const user = await prisma.user.findFirst({
-//           where: {
-//             email: credentials.email,
-//           },
-//         })
-//         if (!user) {
-//           return null;
-//         }
-//         const passwordMatch = await validatePassword(credentials.password, user?.password);
-//         if (!passwordMatch) {
-//           return null;
-//         }
-//         return {
-//           id: user.id,
-//           name: user.username,
-//           email: user.email,
-//         };
-//       },
-//     }),
-//   ],
-//   secret: process.env.NEXTAUTH_SECRET || "",
-//   callbacks: {
-//     async jwt({ token, user }: any) {
-//       if (user) {
-//         token.id = user.id;
-//         token.name = user.name;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }: any) {
-//       if (token) {
-//         session.user.id = token.id;
-//         session.user.name = token.name;
-//       }
-//       return session;
-//     },
-//   },
-//   pages: {
-//     signIn: "/signin",
-//     signUp: "/signup",
-//   },
-// };
+export const NEXT_AUTH = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any) {
+        // if (!credentials?.email || !credentials?.password) {
+        //   return null;
+        // }
+        // const user = await prisma.user.findFirst({
+        //   where: {
+        //     email: credentials.email,
+        //   },
+        // })
+        // if (!user) {
+        //   return null;
+        // }
+        // const passwordMatch = await validatePassword(credentials.password, user?.password);
+        // if (!passwordMatch) {
+        //   return null;
+        // }
+        // return {
+        //   id: user.id,
+        //   name: user.username,
+        //   email: user.email,
+        // };
+        return credentials;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET || "",
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+    signUp: "/signup",
+  },
+};
 
-// export const validatePassword = async (
-//   password: string,
-//   hashedPassword: string
-// ) => {
-//   const comparePassword = await bcrypt.compare(password, hashedPassword);
-//   return comparePassword;
-// };
+export const validatePassword = async (
+  password: string,
+  hashedPassword: string
+) => {
+  const comparePassword = await bcrypt.compare(password, hashedPassword);
+  return comparePassword;
+};
